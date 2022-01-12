714ea9318caee244c91b29fc9753837b6e19d69d
Cahue Kokeny <cahue.kokeny@hotmail.com>
2022-01-12 14:11:22 -0500

    first commit

---
+++ Pokedex iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex iOS
+//
+//  Created by user209843 on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
